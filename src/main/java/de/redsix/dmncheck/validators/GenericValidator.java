9d62690cdc45d8b171c42e09d7e67558cfc2c037
Pascal Wittmann <pascal.wittmann@red6-es.de>
2017-11-28 13:03:09 +0100

    Fix error for the case that GenericValidator<T,T>

--- src/main/java/de/redsix/dmncheck/validators/GenericValidator.java
+++ src/main/java/de/redsix/dmncheck/validators/GenericValidator.java
@@ -1,12 +1,13 @@
 package de.redsix.dmncheck.validators;
 
-
 import org.camunda.bpm.model.dmn.DmnModelInstance;
 import org.camunda.bpm.model.xml.instance.ModelElementInstance;
+import org.camunda.bpm.model.xml.type.ModelElementType;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public interface GenericValidator<S extends ModelElementInstance, T extends ModelElementInstance>{
 
@@ -19,9 +20,11 @@ public interface GenericValidator<S extends ModelElementInstance, T extends Mode
     Class<T> getClassUnderValidation();
 
     default List<ValidationResult> apply(final DmnModelInstance dmnModelInstance) {
+        final ModelElementType elementType = dmnModelInstance.getModel().getType(getClassUnderValidation());
         final Collection<S> elements = dmnModelInstance.getModelElementsByType(getClassUsedToCheckApplicability());
-        return elements.stream().filter(this::isApplicable)
-                .flatMap(element -> element.getParentElement().getChildElementsByType(getClassUnderValidation()).stream())
+        return elements.stream().filter(this::isApplicable).flatMap(element -> Stream
+                .concat(element.getChildElementsByType(getClassUnderValidation()).stream(),
+                        element.getElementType().equals(elementType) ? Stream.of((T) element) : Stream.empty()))
                 .flatMap(element -> validate(element).stream()).collect(Collectors.toList());
     }
 }
