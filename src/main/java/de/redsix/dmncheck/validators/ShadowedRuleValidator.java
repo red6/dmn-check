2f043717232ceadcc2045aa2d7d72bcbe2257556
Pascal Wittmann <pascal.wittmann@red6-es.de>
2018-01-30 17:50:20 +0100

    Extract methods in ShadowedRuleValidator

--- src/main/java/de/redsix/dmncheck/validators/ShadowedRuleValidator.java
+++ src/main/java/de/redsix/dmncheck/validators/ShadowedRuleValidator.java
@@ -8,6 +8,7 @@ import de.redsix.dmncheck.util.Util;
 import de.redsix.dmncheck.validators.core.Validator;
 import org.camunda.bpm.model.dmn.HitPolicy;
 import org.camunda.bpm.model.dmn.instance.DecisionTable;
+import org.camunda.bpm.model.dmn.instance.InputEntry;
 import org.camunda.bpm.model.dmn.instance.Rule;
 
 import java.util.ArrayList;
@@ -40,34 +41,68 @@ public enum ShadowedRuleValidator implements Validator<DecisionTable> {
 
         return Util.zip(IntStream.range(1, rules.size()).boxed(), rules.stream(),
                 (n, rule) -> rules.stream().skip(n).flatMap(potentiallySubsumingRule -> {
-                    final List<Either<Optional<Boolean>, ValidationResult.Builder>> foo = Util
-                            .zip(rule.getInputEntries().stream(), potentiallySubsumingRule.getInputEntries().stream(),
-                                    (input, potentiallySubsumingInput) -> FeelParser.parse(input.getTextContent())
-                                            .bind(inputExpression -> FeelParser.parse(potentiallySubsumingInput.getTextContent())
-                                                    .bind(potentiallySubsumingInputExpression -> Eithers
-                                                            .left(potentiallySubsumingInputExpression.subsumes(inputExpression)))))
-                            .collect(Collectors.toList());
+                    final List<Either<Optional<Boolean>, ValidationResult.Builder>> subsumptionCheckResult = checkRulesForSubsumption(rule,
+                            potentiallySubsumingRule);
 
-                    final List<ValidationResult> parsingErrors = foo.stream().map(Eithers::getRight).filter(Optional::isPresent)
-                            .map(Optional::get).map(builder -> builder.extend($ -> {
-                                $.element = rule;
-                            })).map(ValidationResult.Builder::build).collect(Collectors.toList());
+                    final List<ValidationResult> parsingErrors = extractParsingErrors(rule, subsumptionCheckResult);
 
-                    if (parsingErrors.isEmpty()) {
-                        final List<Optional<Boolean>> subsumptionResults = foo.stream().map(Eithers::getLeft).filter(Optional::isPresent)
-                                .map(Optional::get).collect(Collectors.toList());
-                        if (!subsumptionResults.contains(Optional.<Boolean>empty()) && subsumptionResults.stream()
-                                .allMatch(result -> result.orElse(false))) {
-                            return Stream.of(ValidationResult.Builder.with($ -> {
-                                $.message = "Rule is shadowed by rule " + potentiallySubsumingRule.getId();
-                                $.element = rule;
-                            }).build());
-                        } else {
-                            return Stream.empty();
-                        }
-                    } else {
+                    if (!parsingErrors.isEmpty()) {
                         return parsingErrors.stream();
                     }
+
+                    final List<Optional<Boolean>> subsumptionResults = extractSubsumptionResults(subsumptionCheckResult);
+                    if (subsumptionCheckIsPossible(subsumptionResults) && everythingIsSubsumed(subsumptionResults)) {
+                        return Stream.of(ValidationResult.Builder.with($ -> {
+                            $.message = "Rule is shadowed by rule " + potentiallySubsumingRule.getId();
+                            $.element = rule;
+                        }).build());
+                    } else {
+                        return Stream.empty();
+                    }
                 })).flatMap(Function.identity()).collect(Collectors.toList());
+    }
+
+    private List<Either<Optional<Boolean>, ValidationResult.Builder>> checkRulesForSubsumption(final Rule rule,
+            final Rule potentiallySubsumingRule) {
+        return Util
+                .zip(rule.getInputEntries().stream(), potentiallySubsumingRule.getInputEntries().stream(), this::checkInputsForSubsumption)
+                .collect(Collectors.toList());
+    }
+
+    private Either<Optional<Boolean>, ValidationResult.Builder> checkInputsForSubsumption(final InputEntry input,
+            final InputEntry potentiallySubsumingInput) {
+        return FeelParser.parse(input.getTextContent()).bind(inputExpression ->
+                FeelParser.parse(potentiallySubsumingInput.getTextContent()).bind(potentiallySubsumingInputExpression ->
+                        Eithers.left(potentiallySubsumingInputExpression.subsumes(inputExpression))));
+    }
+
+    private List<Optional<Boolean>> extractSubsumptionResults(
+            final List<Either<Optional<Boolean>, ValidationResult.Builder>> subsumptionCheckResult) {
+        return subsumptionCheckResult.stream()
+                .map(Eithers::getLeft)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    private List<ValidationResult> extractParsingErrors(final Rule rule,
+            final List<Either<Optional<Boolean>, ValidationResult.Builder>> subsumptionCheckResult) {
+        return subsumptionCheckResult.stream()
+                .map(Eithers::getRight)
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .map(builder -> builder.extend($ -> {
+                    $.element = rule;
+                }))
+                .map(ValidationResult.Builder::build)
+                .collect(Collectors.toList());
+    }
+
+    private boolean everythingIsSubsumed(final List<Optional<Boolean>> subsumptionResults) {
+        return subsumptionResults.stream().allMatch(result -> result.orElse(false));
+    }
+
+    private boolean subsumptionCheckIsPossible(final List<Optional<Boolean>> subsumptionResults) {
+        return !subsumptionResults.contains(Optional.<Boolean>empty());
     }
 }
