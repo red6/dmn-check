3a1c4184ab988afe111f3e456dccea65a4090b2d
Pascal Wittmann <pascal.wittmann@red6-es.de>
2018-04-18 11:09:00 +0200

    Fix import of gpg key

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-if [ ! -z "$GPG_SECRET_KEYS" ]; then echo $GPG_SECRET_KEYS | base64 --decode | gpg --import; fi
-if [ ! -z "$GPG_OWNERTRUST" ]; then echo $GPG_OWNERTRUST | base64 --decode | gpg --import-ownertrust; fi
-
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
