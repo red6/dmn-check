5ff0cf5e29428e11f553c1b9b362e2cbef28342c
Pascal Wittmann <pascal.wittmann@red6-es.de>
2018-04-18 12:48:47 +0200

    Use release profile when deploying

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,9 +3,9 @@
 if [ ! -z "$TRAVIS_TAG" ]
 then
     echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.5:set -DnewVersion=$TRAVIS_TAG -Prelease 1>/dev/null 2>/dev/null
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U -Prelease
