81af437050a8c8e608db77f65ab9b0c7a10ab174
Pascal Wittmann <pascal.wittmann@red6-es.de>
2019-12-13 20:47:52 +0100

    Move requirement graph construction in class RequirementGraph

--- core/src/main/java/de/redsix/dmncheck/drg/RequirementGraph.java
+++ core/src/main/java/de/redsix/dmncheck/drg/RequirementGraph.java
@@ -1,9 +1,19 @@
 package de.redsix.dmncheck.drg;
 
+import org.camunda.bpm.model.dmn.DmnModelInstance;
+import org.camunda.bpm.model.dmn.instance.AuthorityRequirement;
+import org.camunda.bpm.model.dmn.instance.Decision;
 import org.camunda.bpm.model.dmn.instance.Definitions;
 import org.camunda.bpm.model.dmn.instance.DrgElement;
+import org.camunda.bpm.model.dmn.instance.InformationRequirement;
+import org.camunda.bpm.model.dmn.instance.InputData;
+import org.camunda.bpm.model.dmn.instance.KnowledgeSource;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DirectedAcyclicGraph;
+
+import java.util.Collection;
+import java.util.Objects;
+import java.util.stream.Stream;
 
 public class RequirementGraph extends DirectedAcyclicGraph<DrgElement, DefaultEdge> {
 
@@ -16,5 +26,46 @@ public class RequirementGraph extends DirectedAcyclicGraph<DrgElement, DefaultEd
 
     public Definitions getDefinitions() {
         return definitions;
+    }
+
+    public static RequirementGraph from(final DmnModelInstance dmnModelInstance) throws IllegalArgumentException {
+        final Collection<Decision> decisions = dmnModelInstance.getModelElementsByType(Decision.class);
+        final Collection<KnowledgeSource> knowledgeSources = dmnModelInstance.getModelElementsByType(KnowledgeSource.class);
+        final Collection<InputData> inputData = dmnModelInstance.getModelElementsByType(InputData.class);
+
+        final RequirementGraph drg = new RequirementGraph(DefaultEdge.class, dmnModelInstance.getDefinitions());
+
+        Stream.of(decisions, knowledgeSources, inputData).flatMap(Collection::stream).forEach(drg::addVertex);
+
+        for (Decision decision : decisions) {
+            decision.getInformationRequirements().stream()
+                    .flatMap(RequirementGraph::collectDrgElements)
+                    .forEach(drgElement -> drg.addEdge(drgElement, decision));
+
+            decision.getAuthorityRequirements().stream()
+                    .flatMap(RequirementGraph::collectDrgElements)
+                    .forEach(drgElement -> drg.addEdge(drgElement, decision));
+        }
+
+        for (KnowledgeSource knowledgeSource : knowledgeSources) {
+            knowledgeSource.getAuthorityRequirement().stream()
+                           .flatMap(RequirementGraph::collectDrgElements)
+                           .forEach(drgElement -> drg.addEdge(drgElement, knowledgeSource));
+        }
+
+        return drg;
+    }
+
+
+    private static Stream<DrgElement> collectDrgElements(InformationRequirement informationRequirement) {
+        return Stream.of(informationRequirement.getRequiredDecision(), informationRequirement.getRequiredInput())
+                     .filter(Objects::nonNull);
+    }
+
+    private static Stream<DrgElement> collectDrgElements(AuthorityRequirement authorityRequirement) {
+        return Stream.of(authorityRequirement.getRequiredDecision(),
+                         authorityRequirement.getRequiredInput(),
+                         authorityRequirement.getRequiredAuthority())
+                     .filter(Objects::nonNull);
     }
 }
--- core/src/main/java/de/redsix/dmncheck/validators/core/RequirementGraphValidator.java
+++ core/src/main/java/de/redsix/dmncheck/validators/core/RequirementGraphValidator.java
@@ -3,19 +3,9 @@ package de.redsix.dmncheck.validators.core;
 import de.redsix.dmncheck.drg.RequirementGraph;
 import de.redsix.dmncheck.result.ValidationResult;
 import org.camunda.bpm.model.dmn.DmnModelInstance;
-import org.camunda.bpm.model.dmn.instance.AuthorityRequirement;
-import org.camunda.bpm.model.dmn.instance.Decision;
-import org.camunda.bpm.model.dmn.instance.DrgElement;
-import org.camunda.bpm.model.dmn.instance.InformationRequirement;
-import org.camunda.bpm.model.dmn.instance.InputData;
-import org.camunda.bpm.model.dmn.instance.KnowledgeSource;
-import org.jgrapht.graph.DefaultEdge;
 
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Stream;
 
 /**
  * Generic validator that facilitates writing validations on graph-based representation of the DMN model instance.
@@ -33,49 +23,15 @@ public abstract class RequirementGraphValidator implements Validator {
 
     @Override
     public List<ValidationResult> apply(DmnModelInstance dmnModelInstance) {
-        final Collection<Decision> decisions = dmnModelInstance.getModelElementsByType(Decision.class);
-        final Collection<KnowledgeSource> knowledgeSources = dmnModelInstance.getModelElementsByType(KnowledgeSource.class);
-        final Collection<InputData> inputData = dmnModelInstance.getModelElementsByType(InputData.class);
-
-        final RequirementGraph drg = new RequirementGraph(DefaultEdge.class, dmnModelInstance.getDefinitions());
-
-        Stream.of(decisions, knowledgeSources, inputData).flatMap(Collection::stream).forEach(drg::addVertex);
-
         try {
-            for (Decision decision : decisions) {
-                decision.getInformationRequirements().stream()
-                        .flatMap(this::collectDrgElements)
-                        .forEach(drgElement -> drg.addEdge(drgElement, decision));
-
-                decision.getAuthorityRequirements().stream()
-                        .flatMap(this::collectDrgElements)
-                        .forEach(drgElement -> drg.addEdge(drgElement, decision));
-            }
-
-            for (KnowledgeSource knowledgeSource : knowledgeSources) {
-                knowledgeSource.getAuthorityRequirement().stream()
-                        .flatMap(this::collectDrgElements)
-                        .forEach(drgElement -> drg.addEdge(drgElement, knowledgeSource));
-            }
+            final RequirementGraph requirementGraph = RequirementGraph.from(dmnModelInstance);
+            return validate(requirementGraph);
         } catch (IllegalArgumentException exception) {
             return Collections.singletonList(ValidationResult.init
-                    .message("Error while construction requirement graph: " + exception.getMessage())
-                    .element(dmnModelInstance.getDefinitions())
-                    .build());
+                                                     .message("Error while construction requirement graph: " + exception.getMessage())
+                                                     .element(dmnModelInstance.getDefinitions())
+                                                     .build());
         }
-
-        return validate(drg);
     }
 
-    private Stream<DrgElement> collectDrgElements(InformationRequirement informationRequirement) {
-        return Stream.of(informationRequirement.getRequiredDecision(), informationRequirement.getRequiredInput())
-                .filter(Objects::nonNull);
-    }
-
-    private Stream<DrgElement> collectDrgElements(AuthorityRequirement authorityRequirement) {
-        return Stream.of(authorityRequirement.getRequiredDecision(),
-                authorityRequirement.getRequiredInput(),
-                authorityRequirement.getRequiredAuthority())
-                .filter(Objects::nonNull);
-    }
 }
