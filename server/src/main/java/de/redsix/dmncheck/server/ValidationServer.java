4dec315a2066b52bbe2de78b67eeddbb552053a3
Pascal Wittmann <pascal.wittmann@red6-es.de>
2020-02-24 11:09:36 +0100

    Handle runtime exceptions when construction dmn model

--- server/src/main/java/de/redsix/dmncheck/server/ValidationServer.java
+++ server/src/main/java/de/redsix/dmncheck/server/ValidationServer.java
@@ -2,7 +2,9 @@ package de.redsix.dmncheck.server;
 
 import de.redsix.dmncheck.result.ValidationResult;
 import de.redsix.dmncheck.util.ValidatorLoader;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.camunda.bpm.model.dmn.Dmn;
+import org.camunda.bpm.model.dmn.DmnModelException;
 import org.camunda.bpm.model.dmn.DmnModelInstance;
 
 import java.io.ByteArrayInputStream;
@@ -20,9 +22,15 @@ public class ValidationServer {
         post("/validate", (request, response) -> {
 
             final ByteArrayInputStream dmnXmlStream = new ByteArrayInputStream(request.body().getBytes(StandardCharsets.UTF_8));
-            final DmnModelInstance modelInstance = Dmn.readModelFromStream(dmnXmlStream);
 
-            return validationServer.runValidators(modelInstance).toString();
+            try {
+                final DmnModelInstance modelInstance = Dmn.readModelFromStream(dmnXmlStream);
+                return validationServer.runValidators(modelInstance).toString();
+            } catch (DmnModelException e) {
+                return ExceptionUtils.getRootCause(e).getMessage();
+            } catch (Exception e) {
+                return e.getMessage();
+            }
         });
     }
 
