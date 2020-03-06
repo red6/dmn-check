618580e1a9aaff2548e536fe2ec36012db0b7369
Pascal Wittmann <pascal.wittmann@red6-es.de>
2020-03-06 12:25:23 +0100

    Implemented a Gradle plugin for DMN validation
    The Gradle plugin follows the Maven plugin and you should see the same
    validation results with the same configuration parameters.
    
    You can use the plugin in the following way:
    
        apply plugin: 'de.redsix.dmncheck.plugin'
    
        dmnCheck {
          searchPathList =  [ "src/" ]
          excludeList = []
          validatorPackages = [ "de.redsix.dmncheck.validators" "de.redsix.dmncheck.validators.core" ]
          validatorClasses = [ "de.redsix.dmncheck.validators.ShadowedRuleValidator" ]
        }
    
    Note: All configuration paramters are optional.

---
+++ gradle-plugin/src/main/java/de/redsix/dmncheck/DmnCheckGradlePlugin.java
@@ -0,0 +1,19 @@
+package de.redsix.dmncheck;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public class DmnCheckGradlePlugin implements Plugin<Project> {
+
+    public static final String DMN_CHECK_EXTENSION = "dmnCheck";
+    public static final String CHECK_DMN_TASK = "checkDmn";
+
+    @Override
+    public void apply(Project project) {
+        final DmnCheckExtension dmnCheckExtension = project.getExtensions().create(DMN_CHECK_EXTENSION, DmnCheckExtension.class);
+        final DmnCheckTask dmnCheckTask = project.getTasks().create(CHECK_DMN_TASK, DmnCheckTask.class);
+
+        dmnCheckTask.getExtensions().add(DMN_CHECK_EXTENSION, dmnCheckExtension);
+        dmnCheckTask.setGroup("verification");
+    }
+}
