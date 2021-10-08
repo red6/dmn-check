5d3666b39e01c45b75156f91f6b1f2c577a2d4df
Pascal Wittmann <pascal.wittmann@red6-es.de>
2021-10-09 00:29:16 +0200

    Add comment about the NonValidatingDmnParser

--- server/src/main/java/de/redsix/dmncheck/server/NonValidatingDmnParser.java
+++ server/src/main/java/de/redsix/dmncheck/server/NonValidatingDmnParser.java
@@ -7,7 +7,8 @@ public class NonValidatingDmnParser extends DmnParser {
 
     @Override
     public void validateModel(DomDocument document) {
-
+        // Do not validate against the xml schema for now. Errors from the xml validation are hard to map back
+        // into the editor.
     }
 
 }
