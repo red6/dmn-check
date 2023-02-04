5586404db1018dddcc028b2fe885465074a262b2
Pascal Wittmann <pascal.wittmann@red6-es.de>
2023-02-04 13:21:31 +0100

    Add tests for cli module

---
+++ cli/src/test/java/de/redsix/dmncheck/cli/MainTest.java
@@ -0,0 +1,85 @@
+package de.redsix.dmncheck.cli;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    CommandLine cmd;
+
+    @BeforeEach
+    void setupCommand() {
+        Main app = new Main();
+        cmd = new CommandLine(app);
+
+    }
+
+    @Test
+    void shouldPrintHelp() {
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+
+        int exitCode = cmd.execute("--help");
+        assertEquals(0, exitCode);
+        assertTrue(sw.toString().startsWith("Usage:"));
+    }
+
+    @Test
+    void shouldSetExcludeList() {
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+
+        int exitCode = cmd.execute("--excludeList=some-file.dmn");
+        Main main = cmd.getCommand();
+        assertEquals(0, exitCode);
+        assertEquals(List.of("some-file.dmn"), main.getExcludeList());
+    }
+
+    @Test
+    void shouldSetSearchPath() {
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+
+        cmd.execute("--searchPath=model/");
+        Main main = cmd.getCommand();
+        assertEquals(List.of("model/"), main.getSearchPathList());
+    }
+
+    @Test
+    void shouldSetValidatorPackages() {
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+
+        cmd.execute("--validatorPackages=com.red6.dmn");
+        Main main = cmd.getCommand();
+        assertArrayEquals(List.of("com.red6.dmn").toArray(new String[0]), main.getValidatorPackages());
+    }
+
+    @Test
+    void shouldSetValidatorClasses() {
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+
+        cmd.execute("--validatorClasses=FancyValidator,DreamValidator");
+        Main main = cmd.getCommand();
+        assertArrayEquals(List.of("FancyValidator", "DreamValidator").toArray(new String[0]), main.getValidatorClasses());
+    }
+
+    @Test
+    void shouldSetFailOnWarning() {
+        StringWriter sw = new StringWriter();
+        cmd.setOut(new PrintWriter(sw));
+
+        cmd.execute("--failOnWarning");
+        Main main = cmd.getCommand();
+        assertTrue(main.failOnWarning());
+    }
+
+}
